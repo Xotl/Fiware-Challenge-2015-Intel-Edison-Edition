7eca9d282f60b45b79c853588e65ad5c4a155c9c
Edgardo Campos (Xotl) <edgardo333@gmail.com>
2015-08-15 14:43:54 -0500

    First working version

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-var mraa = require('mraa'), //require mraa
+var mraa = require('mraa') //require mraa
     request = require('request');
 
 
-var button = new mraa.Gpio(6);// pin J17-7
-    myLed = new m.Gpio(13); // pin J17-14
+var button = new mraa.Gpio(6), // pin J17-7
+    myLed = new mraa.Gpio(13), // pin J17-14
+    BUTTON_DOWN = 0,
+    BUTTON_UP= 1;
+
+
 
 
 button.dir(mraa.DIR_IN); //set the gpio direction to input
-myLed.dir(m.DIR_OUT); //set the gpio direction to output
+myLed.dir(mraa.DIR_OUT); //set the gpio direction to output
 myLed.write(0);// Make sure is turned off.
 
 var fastBlink = function (callback) {
@@ -29,22 +33,23 @@ var fastBlink = function (callback) {
 };
 
 var sendDataAndrestart = function(liters) {
-
   var blink = 1;
   var ultraFastBlinkInterval = setInterval(function() {
-    myLed.write(ledState ? 1 : 0);
+    myLed.write(blink ? 1 : 0);
   }, 200);
 
 
+  console.log('Sending data...');
   // Send data
-  request.get('http://reto-fiware-cp15.herokuapp.com/api/postUserData', { liters: liters })
-  .on('response', function(response) {
+  request.get('http://reto-fiware-cp15.herokuapp.com/api/postUserData?liters=' + liters)
+  .on('response', function(response, body) {
     clearInterval(ultraFastBlinkInterval);
     myLed.write(0);// Make sure is turned off.
     if (response.statusCode != 200) {
-      console.log(response);
+      console.log('An Error ' + response.statusCode + ' received from server');
     }
 
+    console.log('Data sent, restarting.');
     waitForWakeUp();
   })
   .on('error', function(err) {
@@ -56,29 +61,36 @@ var sendDataAndrestart = function(liters) {
 };
 
 var waitForWakeUp = function() {
-  var newState = button.read(); //read the digital value of the pin
 
-  if (newState === 0) {
+  if (button.read() === BUTTON_UP) {
+    console.log('Waiting to wake up...');
     // Continue waiting
     setTimeout(waitForWakeUp, 2000); //call the indicated function after 1 second (1000 milliseconds)
     return;
   }
 
+  console.log('Waked up!');
+
+
   // Wake Up signal to User (fast blink)
   fastBlink(function () {
 
+    console.log('Waiting to received gas...');
     // Wait for the gas filling
     var waitForStartFillingInterval = setInterval(function() {
-      if (button.read() === 1) {
+
+      if (button.read() === BUTTON_DOWN) {
         myLed.write(0);
         clearInterval(waitForStartFillingInterval);
+
+        console.log('Receiving gas!');
 
         // Count mililiters (1000 ms == 1 liter of gas)
         var gasMililiters = 0;
         var fillingInterval = setInterval(function() {
-          if (button.read() === 0) {
+          if (button.read() === BUTTON_UP) {
             clearInterval(fillingInterval);
-            sendDataAndrestart();
+            sendDataAndrestart(gasMililiters / 1000);
             return;
           }
 
